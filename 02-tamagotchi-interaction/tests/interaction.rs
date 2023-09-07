771db2d3d2e480d3c8a868d498beb844f00e9e4b
Yury <yury.yukhananov@tutanota.com>
2023-09-07 12:25:33 -0300

    2nd assignment

--- 02-tamagotchi-interaction/tests/interaction.rs
+++ 02-tamagotchi-interaction/tests/interaction.rs
@@ -1,12 +1,58 @@
-use gtest::{Program, System};
+use gtest::{Program, System, Log};
+use tmg2_io::{Tamagotchi, TmgAction, TmgEvent};
 
-// TODO: 0️⃣ Copy tests from the previous lesson and push changes to the master branch
-
+// Yury: this is just a basic test
+// I didn't find if it's possible to manually advance blocks in the mock environment for testing Mood changes
+// The printouts always return default values, regardless if they are previously initialized, it's unclear to why.
+// However I tested the contract functionality on the testnet by sending relevant messages manually.
 #[test]
 fn interaction_test() {
     let sys = System::new();
     sys.init_logger();
-    let _program = Program::current(&sys);
+    let program = Program::current(&sys);
 
-    // TODO: 6️⃣ Test new functionality
+    let res = program.send(2, "Tama".to_string());
+    assert!(!res.main_failed());
+
+    let res = program.send(2, TmgAction::Name);
+    
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Name("????".to_string()));
+    assert!(res.contains(&expected_log));
+
+    let name = res.decoded_log::<String>();
+    println!("decoded_reply: {:#?}", name);
+
+    let res = program.send(2, TmgAction::Age); 
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Age(5));
+    assert!(res.contains(&expected_log));
+    let age = res.decoded_log::<u64>();
+    println!("age: {:#?} ms", age);
+
+    let res = program.send(2, TmgAction::Feed); 
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Fed);
+    assert!(res.contains(&expected_log));
+
+    let res = program.send(2, TmgAction::Entertain); 
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Entertained);
+    assert!(res.contains(&expected_log));
+    
+    let res = program.send(2, TmgAction::Sleep); 
+    let expected_log = Log::builder()
+        .dest(2)
+        .payload(TmgEvent::Slept);
+    assert!(res.contains(&expected_log));
+
+    let res = program.send(2, TmgAction::Full); 
+    let tmg = res.decoded_log::<Tamagotchi>();
+    println!("FULL: {:#?}", tmg);
+
+
 }
