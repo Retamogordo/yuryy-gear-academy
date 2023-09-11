dae8dfec3c552c4ce23aa9bf3703fb590e317beb
Yury <yury.yukhananov@tutanota.com>
2023-09-11 15:04:06 -0300

    copy changes from 02-tamagotchi-interaction to 03-tamagotchi-nft

--- 03-tamagotchi-nft/io/src/lib.rs
+++ 03-tamagotchi-nft/io/src/lib.rs
@@ -2,34 +2,97 @@
 
 use codec::{Decode, Encode};
 use gmeta::Metadata;
-use gstd::prelude::*;
+use gstd::{ActorId, prelude::*};
 use scale_info::TypeInfo;
+use gmeta::{In, InOut};
 
-#[derive(Default, Encode, Decode, TypeInfo)]
+pub const HUNGER_PER_BLOCK: u64 = 1;
+pub const BOREDOM_PER_BLOCK: u64 = 2;
+pub const ENERGY_PER_BLOCK: u64 = 2;
+pub const FILL_PER_FEED: u64 = 1000;
+pub const FILL_PER_ENTERTAINMENT: u64 = 1000;
+pub const FILL_PER_SLEEP: u64 = 1000;
+
+#[derive(Clone, Default, Encode, Decode, TypeInfo, Debug)]
 pub struct Tamagotchi {
-    // TODO: 0️⃣ Copy fields from previous lesson and push changes to the master branch
-    // TODO: 1️⃣ Add new fields
+    pub name: String,
+    pub date_of_birth: u64,
+    pub owner: ActorId,
+    pub fed: Mood,
+    pub fed_block: u64,
+    pub entertained: Mood,
+    pub entertained_block: u64,
+    pub slept: Mood,
+    pub slept_block: u64,
+}
+
+#[derive(Clone, Encode, Decode, TypeInfo, Debug)]
+pub struct Mood(u64);
+
+impl Mood {
+    pub fn try_add(&mut self, n: u64) -> Result<(), ()> {
+        let tmp = self.0 + n;
+        if tmp < 10_000 {
+            self.0 = tmp;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn try_sub(&mut self, n: u64) -> Result<(), ()> {
+        if self.0 > n {
+            self.0 -= n;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl TryFrom<u64> for Mood {
+    type Error = ();
+    fn try_from(n: u64) -> Result<Self, ()> {
+        if n > 0 && n < 10_000 {
+            Ok(Self(n))
+        } else {
+            Err(())
+        }
+    }
+} 
+
+impl Default for Mood {
+    fn default() -> Self {
+        Self(1)
+    }
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum TmgAction {
-    // TODO: 0️⃣ Copy actions from previous lesson and push changes to the master branch
-    // TODO: 2️⃣ Add new actions
+    Name,
+    Age,
+    Feed,
+    Entertain,
+    Sleep,
+    Full,
 }
 
 #[derive(Encode, Decode, TypeInfo)]
 pub enum TmgEvent {
-    // TODO: 0️⃣ Copy events from previous lesson and push changes to the master branch
-    // TODO: 3️⃣ Add new events
+    Name(String),
+    Age(u64),
+    Fed,
+    Entertained,
+    Slept,
+    Full(Tamagotchi)
 }
 
 pub struct ProgramMetadata;
 
-// TODO: 0️⃣ Copy `Metadata` from the first lesson and push changes to the master branch
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
+    type State = Tamagotchi;
     type Reply = ();
     type Others = ();
     type Signal = ();
--- 03-tamagotchi-nft/src/lib.rs
+++ 03-tamagotchi-nft/src/lib.rs
@@ -1,18 +1,78 @@
 #![no_std]
 
-use gstd::prelude::*;
+use gstd::{exec, msg, prelude::*};
+use tmg2_io::{
+    Tamagotchi, TmgAction, TmgEvent, 
+    HUNGER_PER_BLOCK, BOREDOM_PER_BLOCK, ENERGY_PER_BLOCK, 
+    FILL_PER_FEED, FILL_PER_ENTERTAINMENT, FILL_PER_SLEEP  
+};
+
+static mut TAMAGOTCHI: Option<Tamagotchi> = None;
+static mut PREV_HANDLED_BLOCK: u64 = 0;
 
 #[no_mangle]
 extern "C" fn init() {
-    // TODO: 0️⃣ Copy the `init` function from the previous lesson and push changes to the master branch
+    let name: String = msg::load().expect("Can't load init message");
+    let current_block = exec::block_height() as u64;
+    unsafe { PREV_HANDLED_BLOCK = current_block };
+
+    unsafe { TAMAGOTCHI = Some(
+        Tamagotchi {
+            name,
+            date_of_birth: exec::block_timestamp(),
+            owner: msg::source(),
+            fed: Default::default(),
+            fed_block: current_block,
+            entertained: Default::default(),
+            entertained_block: current_block,
+            slept: Default::default(),
+            slept_block: current_block, 
+        })
+    };
 }
 
 #[no_mangle]
 extern "C" fn handle() {
-    // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    let action: TmgAction = msg::load().expect("Error on loading Tamagotchi Action");
+    let mut tmg: &mut Tamagotchi = unsafe { TAMAGOTCHI.as_mut().expect("Contract not initialized") };
+    let prev_handled_block = unsafe { PREV_HANDLED_BLOCK };
+    let current_block = exec::block_height() as u64;
+
+    // do not fail on trying subtraction, rather leave it at default
+    let _ = tmg.fed.try_sub( (current_block - prev_handled_block) * HUNGER_PER_BLOCK );
+    let _ = tmg.entertained.try_sub( (current_block - prev_handled_block) * BOREDOM_PER_BLOCK );
+    let _ = tmg.slept.try_sub( (current_block - prev_handled_block) * ENERGY_PER_BLOCK );
+
+    match action {
+        TmgAction::Name => msg::reply(TmgEvent::Name(tmg.name.clone()), 0),
+        TmgAction::Age => msg::reply(
+            TmgEvent::Age(exec::block_timestamp() - tmg.date_of_birth), 0
+        ),
+        TmgAction::Feed => {
+            tmg.fed.try_add(FILL_PER_FEED).expect("Fed value is out of range");
+            tmg.fed_block = current_block;
+            msg::reply(TmgEvent::Fed, 0)
+        },
+        TmgAction::Entertain => {
+            tmg.entertained.try_add(FILL_PER_ENTERTAINMENT).expect("Entertained value is out of range");
+            tmg.entertained_block = current_block;
+            msg::reply(TmgEvent::Entertained, 0)
+        },
+        TmgAction::Sleep => {
+            tmg.slept.try_add(FILL_PER_SLEEP).expect("Slept value is out of range");
+            tmg.slept_block = current_block;
+            msg::reply(TmgEvent::Slept, 0)
+        },
+        TmgAction::Full => msg::reply(TmgEvent::Full(tmg.clone()), 0),
+    }
+    .expect("Failed replying to sender");
+
+    unsafe { PREV_HANDLED_BLOCK = current_block as u64 };
 }
 
 #[no_mangle]
 extern "C" fn state() {
-    // TODO: 0️⃣ Copy the `handle` function from the previous lesson and push changes to the master branch
+    msg::reply(
+        unsafe { TAMAGOTCHI.as_ref().expect("Contract not initialized") }, 0
+    ).expect("Unable to return Tamagotchi instance");
 }
